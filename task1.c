7a99df532f31b64ce174765fc54ac80523cf5b5d
priyavaghasiya23 <159774397+priyavaghasiya23@users.noreply.github.com>
2024-02-12 16:46:27 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+main(){
+	printf("my Name is priya\n");
+	printf("Age = 19\n");
+	printf("school name = dipak school");
+}
---
+++ task4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main(){
+	
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*              *  *                 *\n");
+	printf("*           *        *            *\n");
+	printf("*        *              *       *\n");
+	printf("*      *                   * *\n");
+	printf("*    *\n");
+	printf("*\n");
+	
+}
+
+
+
+//*
+//* 
+//* 
+//*                   *   *                        *
+//*               *           *                 *
+//*           *                   *         *
+//*       *                           *  *
+//*    *
+//*
