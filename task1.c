497a91dfef33e1c2529ec4095ef1f3b0dd5a2803
priyavaghasiya23 <159774397+priyavaghasiya23@users.noreply.github.com>
2024-02-15 13:55:15 +0530

    Add files via upload

--- task1.c
+++ task1.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 main(){
-	printf("my Name is priya\n");
-	printf("Age = 19\n");
-	printf("school name = dipak school");
+	
+	int a,b,ans;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	ans=a+b;
+	printf("addition of %d and %d is : %d\n",a,b,ans);
+	ans=a-b;
+	printf("Subtraction of %d and %d is : %d\n",a,b,ans);
+	ans=a*b;
+	printf("Multiplication of %d and %d is : %d\n",a,b,ans);
+	ans=a/b;
+	printf("Division of %d and %d is : %d\n",a,b,ans);
+	ans=a%b;
+	printf("Modulo of %d and %d is : %d\n",a,b,ans);
+	
+	
 }
--- task2.c
+++ task2.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 main(){
-	printf("---------\n");
-	printf("|        |\nR        |\nN        |\nW        |\n|        |\n---------");
+	
+	int a,b,ans;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+	ans=a+b;
+	printf("addition of %d and %d is : %d\n",a,b,ans);
+	printf("%d + %d = %d\n",a,b,ans);
+	
+	ans=a-b;
+	printf("Subtraction of %d and %d is : %d\n",a,b,ans);
+	printf("%d - %d = %d\n",a,b,ans);
+	
+	ans=a*b;
+	printf("Multiplication of %d and %d is : %d\n",a,b,ans);
+	printf("%d * %d = %d\n",a,b,ans);
+	
+	ans=a/b;
+	printf("Division of %d and %d is : %d\n",a,b,ans);
+	printf("%d / %d = %d\n",a,b,ans);
+	
+	ans=a%b;
+	printf("Modulo of %d and %d is : %d\n",a,b,ans);
+	printf("%d % %d = %d\n",a,b,ans);
+	
+	
 }
